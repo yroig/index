d0e22681d4f42eee13a6b37e2255a12c4ae90103
yroig <yroigg@fp.insjoaquimmir.cat>
2020-10-14 17:37:43 +0200

    act3

--- a4/printa_formulari.php
+++ a4/printa_formulari.php
@@ -1,8 +1,26 @@
 
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
+echo "Benvingut".$_REQUEST["apellido"]."<br>";
+if(isset($_REQUEST["colors"])){
+    print_r($_REQUEST["colors"]);
+}
+
+if(isset($_REQUEST["sexe"])){
+    print_r($_REQUEST["sexe"]);
+}
+
+$dir_subida = 'imatges/';
+$fichero_subido = $dir_subida . basename($_FILES['archivo']['name']);
+
+if (move_uploaded_file($_FILES['archivo']['tmp_name'], $fichero_subido)){
+    echo "ok, <a href=\"".$fichero_subido."\">link</a>";
+}
+
 
 } else {
+    echo "ko";
+}
 
 ?>
 
@@ -11,7 +29,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-<title>Exemple de formulari</title>
+<title>Formulari</title>
 
 </head>
 
@@ -19,7 +37,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <div style="margin: 30px 10%;">
 <h3>My form</h3>
-<form action="processa_dades.php" method="post" id="myform" name="myform">
+<form action="printa_formulari.php" method="post" id="myform" name="myform">
 
     <label>Text</label> <input type="text" value="" size="30" maxlength="100" name="mytext" id="" /><br /><br />
 
